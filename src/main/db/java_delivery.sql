9d9d9a0b15dc349fec93c1ccc1314623e92a99b9
Santiago Filippini <santifilippini2005@gmail.com>
2026-09-03 23:31:40 -0300

    fix: updated database dump

--- src/main/db/java_delivery.sql
+++ src/main/db/java_delivery.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: javadelivery
+-- Host: localhost    Database: javadelivery
 -- ------------------------------------------------------
 -- Server version	9.6.0
 
@@ -21,7 +21,7 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd94a4558-335b-11f1-b286-0a002700000c:1-710';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'd94a4558-335b-11f1-b286-0a002700000c:1-882';
 
 --
 -- Table structure for table `discount`
@@ -91,11 +91,14 @@ CREATE TABLE `product` (
   `product_id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(100) COLLATE utf8mb3_bin DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `product_type_id` int DEFAULT NULL,
+  `product_type_id` int NOT NULL,
+  `restaurant_id` int NOT NULL,
   PRIMARY KEY (`product_id`),
   KEY `product_type_fk_idx` (`product_type_id`),
-  CONSTRAINT `product_type_fk` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`product_type_id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+  KEY `restaurant_product_fkey_idx` (`restaurant_id`),
+  CONSTRAINT `product_type_fk` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`product_type_id`),
+  CONSTRAINT `restaurant_product_fkey` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`restaurant_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,6 +107,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Jamón crudo, 300g',1290.32,1,1),(2,'Roast beef, 200g',19020.2,3,1),(3,'Fideos a la carbonara',16059.3,2,1),(4,'Tiramisu',12292,4,1),(5,'Milanesa Napolitana',7893.2,3,1);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +148,7 @@ CREATE TABLE `product_type` (
   `product_type_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(80) COLLATE utf8mb3_bin DEFAULT NULL,
   PRIMARY KEY (`product_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,6 +157,7 @@ CREATE TABLE `product_type` (
 
 LOCK TABLES `product_type` WRITE;
 /*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
+INSERT INTO `product_type` VALUES (1,'Embutidos'),(2,'Pastas'),(3,'Carnes'),(4,'Postres');
 /*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,6 +172,7 @@ CREATE TABLE `restaurant` (
   `restaurant_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(80) COLLATE utf8mb3_bin DEFAULT NULL,
   `address` varchar(80) COLLATE utf8mb3_bin DEFAULT NULL,
+  `image_url` varchar(25) COLLATE utf8mb3_bin DEFAULT NULL,
   PRIMARY KEY (`restaurant_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -177,7 +183,7 @@ CREATE TABLE `restaurant` (
 
 LOCK TABLES `restaurant` WRITE;
 /*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
-INSERT INTO `restaurant` VALUES (1,'La Esquina','Av. Pellegrini 1234'),(2,'El Buen Sabor','San Lorenzo 850'),(3,'Don Giuseppe','Italia 456'),(4,'Sabores del Sur','Bv. Oroño 1720'),(5,'La Terraza','Córdoba 2100'),(6,'Parrilla El Fogón','Mendoza 1450'),(7,'Green Garden','Santa Fe 980'),(8,'Mar y Tierra','Entre Ríos 620');
+INSERT INTO `restaurant` VALUES (1,'La Esquina','Av. Pellegrini 1234','uploads/restaurant1.jpg'),(2,'El Buen Sabor','San Lorenzo 850','uploads/restaurant2.jpg'),(3,'Don Giuseppe','Italia 456','uploads/restaurant3.jpg'),(4,'Sabores del Sur','Bv. Oroño 1720','uploads/restaurant4.jpg'),(5,'La Terraza','Córdoba 2100','uploads/restaurant5.jpg'),(6,'Parrilla El Fogón','Mendoza 1450','uploads/restaurant6.jpg'),(7,'Green Garden','Santa Fe 980','uploads/restaurant7.jpg'),(8,'Mar y Tierra','Entre Ríos 620','uploads/restaurant8.jpg');
 /*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +212,7 @@ CREATE TABLE `schedule` (
 
 LOCK TABLES `schedule` WRITE;
 /*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
-INSERT INTO `schedule` VALUES (1,1,'12:00:00','16:00:00','monday'),(1,2,'11:30:00','15:30:00','monday'),(1,3,'12:00:00','15:00:00','tuesday'),(1,4,'20:00:00','23:00:00','friday'),(1,5,'12:00:00','16:00:00','monday'),(1,6,'20:00:00','23:30:00','saturday'),(1,7,'12:00:00','15:00:00','monday'),(1,8,'12:00:00','16:00:00','tuesday'),(2,1,'12:00:00','16:00:00','tuesday'),(2,2,'11:30:00','15:30:00','wednesday'),(2,3,'12:00:00','15:00:00','thursday'),(2,4,'20:00:00','23:00:00','saturday'),(2,5,'12:00:00','16:00:00','tuesday'),(2,7,'12:00:00','15:00:00','thursday'),(2,8,'12:00:00','16:00:00','thursday'),(3,1,'12:00:00','16:00:00','wednesday'),(3,2,'20:00:00','23:30:00','friday'),(3,3,'20:00:00','23:30:00','friday'),(3,4,'17:00:00','21:00:00','tuesday'),(3,5,'12:00:00','16:00:00','wednesday'),(3,7,'12:00:00','15:00:00','friday'),(3,8,'20:00:00','23:30:00','friday'),(4,1,'20:00:00','00:00:00','friday'),(4,3,'20:00:00','23:30:00','saturday'),(4,5,'20:00:00','00:00:00','friday'),(4,7,'16:00:00','19:30:00','tuesday'),(4,8,'20:00:00','23:30:00','sunday'),(5,1,'20:00:00','00:00:00','saturday'),(5,5,'20:00:00','00:00:00','saturday');
+INSERT INTO `schedule` VALUES (1,1,'12:00:00','16:00:00','monday'),(1,2,'11:30:00','18:00:00','monday'),(1,3,'12:00:00','15:00:00','tuesday'),(1,4,'20:00:00','23:00:00','friday'),(1,5,'12:00:00','16:00:00','monday'),(1,6,'20:00:00','23:30:00','saturday'),(1,7,'12:00:00','15:00:00','monday'),(1,8,'12:00:00','16:00:00','tuesday'),(2,1,'12:00:00','16:00:00','tuesday'),(2,2,'11:30:00','15:30:00','wednesday'),(2,3,'12:00:00','15:00:00','thursday'),(2,4,'20:00:00','23:00:00','saturday'),(2,5,'12:00:00','16:00:00','tuesday'),(2,7,'12:00:00','15:00:00','thursday'),(2,8,'12:00:00','16:00:00','thursday'),(3,1,'12:00:00','16:00:00','wednesday'),(3,2,'20:00:00','23:30:00','friday'),(3,3,'20:00:00','23:30:00','friday'),(3,4,'17:00:00','21:00:00','tuesday'),(3,5,'12:00:00','16:00:00','wednesday'),(3,7,'12:00:00','15:00:00','friday'),(3,8,'20:00:00','23:30:00','friday'),(4,1,'20:00:00','00:00:00','friday'),(4,3,'20:00:00','23:30:00','saturday'),(4,5,'20:00:00','00:00:00','friday'),(4,7,'16:00:00','19:30:00','tuesday'),(4,8,'20:00:00','23:30:00','sunday'),(5,1,'20:00:00','00:00:00','saturday'),(5,5,'20:00:00','00:00:00','saturday');
 /*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -229,7 +235,7 @@ CREATE TABLE `user` (
   `role` enum('admin','client') COLLATE utf8mb3_bin NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,6 +244,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'a@a','a','a','a','a','a','a','client'),(2,'b@b','b','b','b','b','b','b','client'),(3,'c@c','c','c','c','c','c','c','client'),(4,'Hola@hola','Hola','Chau','hola','asdasdl','oasd','JEJE','client'),(5,'pepe@gmail.com','pepe','sand','pepe','2390239203','20202020','Villa 1-11-14','client');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -257,11 +264,9 @@ CREATE TABLE `user_order` (
   `restaurant_id` int NOT NULL,
   PRIMARY KEY (`order_id`),
   KEY `discount_fk_idx` (`discount_id`),
-  KEY `user_fk_idx` (`user_id`),
   KEY `restaurant_fk_idx` (`restaurant_id`),
   CONSTRAINT `discount_fk` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`discount_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `restaurant_id_fk` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`restaurant_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `restaurant_id_fk` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`restaurant_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -284,4 +289,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-26 10:03:28
+-- Dump completed on 2026-09-03 23:30:39
